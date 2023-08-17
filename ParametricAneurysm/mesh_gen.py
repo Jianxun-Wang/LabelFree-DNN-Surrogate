75cab9c2eba7bcc16b984527c07cff993d217e74
Aseem <aseemshehzad10@gmail.com>
2023-08-17 11:57:14 -0400

    Refactored ParametricAneurym scripts for better readability and performance

--- ParametricAneurysm/mesh_gen.py
+++ ParametricAneurysm/mesh_gen.py
@@ -1,12 +1,19 @@
-import numpy as np 
-def ThreeD_mesh(x_1d,x_2d,tmp_1d,sigma,mu):
-	tmp_3d = np.expand_dims(np.tile(tmp_1d,len(x_2d)),1).astype('float')
-	x = []
+import numpy as np
 
-	for x0 in x_2d:
-		  tmpx = np.tile(x0,len(tmp_1d))
-		  x.append(tmpx)
+def create_3d_mesh(x_values, mesh_2d, z_values):
+    """
+    Create a 3D mesh based on given x_axis values, 2D mesh, and z_axis values.
 
-	x = np.reshape(x,(len(tmp_3d),1))
+    Args:
+    - x_values: A 1D numpy array containing x values.
+    - mesh_2d: A 2D numpy array containing flatted x and y values.
+    - z_values: A 1D numpy array containing z values.
 
-	return x,tmp_3d
+    Returns:
+    - y_mesh: A 2D numpy array containing y values for the 3D mesh.
+    - z_mesh: A 2D numpy array containing z values for the 3D mesh.
+    """
+    tmp_3d = np.expand_dims(np.tile(z_values, len(mesh_2d)), 1).astype('float')
+    mesh_3d = np.expand_dims(np.repeat(mesh_2d, len(z_values)), axis=1)
+
+    return mesh_3d, tmp_3d
